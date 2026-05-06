80d236af856747beb2d26cd8940f70d916e00bb8
Bharath.1o <frozenbmg@gmail.com>
2026-05-07 01:50:14 +0530

    plagiarism.py
    Added download for 'punkt_tab' to NLTK.

--- backend/plagiarism.py
+++ backend/plagiarism.py
@@ -2,6 +2,7 @@ from difflib import SequenceMatcher
 import nltk
 
 nltk.download('punkt')
+nltk.download('punkt_tab')
 
 database_sentences = [
     "Artificial intelligence is transforming healthcare.",
@@ -13,22 +14,31 @@ def similarity(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 def check_plagiarism(text):
+
     uploaded_sentences = nltk.sent_tokenize(text)
 
     plagiarism_results = []
 
     for sentence in uploaded_sentences:
+
         for db_sentence in database_sentences:
 
-            score = similarity(sentence.lower(), db_sentence.lower())
+            score = similarity(
+                sentence.lower(),
+                db_sentence.lower()
+            )
 
             if score > 0.7:
+
                 plagiarism_results.append({
                     "uploaded_sentence": sentence,
                     "matched_sentence": db_sentence,
                     "similarity_score": round(score * 100, 2)
                 })
 
-    originality_score = max(0, 100 - (len(plagiarism_results) * 10))
+    originality_score = max(
+        0,
+        100 - (len(plagiarism_results) * 10)
+    )
 
     return originality_score, plagiarism_results
